1e212a12bb313070fc42a1e78c5bd267b96f31a5
Rohit2031 <70621663+Rohit2031@users.noreply.github.com>
2020-09-06 09:42:29 +0530

    Add files via upload

---
+++ react-navbar/src/components/Navbar/Menu.js
@@ -0,0 +1,33 @@
+export const MenuItems = [
+   
+    {
+        title:'TRANINEE',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'TEXTFIELD DEMO',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'INPUT DEMO',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'CHILDREN DEMO',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'SING UP ',
+        url:'#',
+        cName:'nav-links-mobile'
+    },
+    {
+        title:'LOGOUT ',
+        url:'#',
+        cName:'nav-links'
+    }
+]
